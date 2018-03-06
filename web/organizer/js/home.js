4e404970f0555c0ecd00f36786ba084fbc8cc2fd
natthapach <natthapach.a@ku.th>
2018-03-06 23:36:10 +0700

    organizer:home-edit id column

--- web/organizer/js/home.js
+++ web/organizer/js/home.js
@@ -4,21 +4,21 @@ $(document).ready(function(e){
         url:"../../service/organizer/getUserEvent.php",
         dataType:"json",
         success:(res)=>{
-            // res.forEach(element => {
-            //    addEvent(element);
-            // });
-            for(let i=0; i<res.length; i++){
-                addEvent(i+1, res[i]);
-            }
+            res.forEach(element => {
+               addEvent(element);
+            });
+        //     for(let i=0; i<res.length; i++){
+        //         addEvent(i+1, res[i]);
+        //     }
         }
     })
 });
 
-function addEvent(i, event){
+function addEvent(event){
     tbody = $("tbody.event-list");
     console.log(tbody.children());
     tr = tbody.append("<tr>adsa</tr>").children().last();
-    tr.append("<th scope='row'>" + i + "</th>")
+    tr.append("<th scope='row'>" + event.id + "</th>")
         .append("<td>" + event.name + "</td>")
         .append("<td>" + moment(event.event_start_date).format("D MMMM YYYY") + "</td>")
         .append("<td>attendant</td>");
