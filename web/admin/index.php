00c858cb684a5d381f1187d609c07f8a06450b2c
choleeniiz <36328135+choleeniiz@users.noreply.github.com>
2018-03-10 01:25:33 +0700

    edit register

--- web/admin/index.php
+++ web/admin/index.php
@@ -1,4 +1,4 @@
-<?php include("../../service/admin/server.php");?>
+<?php include("server.php");?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,7 +9,7 @@
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="../app.css">
+    <link rel="stylesheet" href="app.css">
     <link rel="stylesheet" href="style.css">
  
 </head>
@@ -61,7 +61,7 @@
         </div>
         
         <form method="post" action="index.php">
-                <?php include("../../service/admin/errors.php"); ?>
+                <?php include("errors.php"); ?>
     
                 <div class="input-group">
                     <label>Username</label>
@@ -71,7 +71,7 @@
             
                 <div class="input-group">
                     <label>Password</label>
-                    <input type="text" name="password">
+                    <input type="password" name="password">
                 </div>
             
     
