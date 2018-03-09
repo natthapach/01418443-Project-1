404fe82597f223ada87ff08fc13e2c7103a73f92
natthapach <natthapach.a@ku.th>
2018-03-09 13:36:19 +0700

    organizer:modify picture uploader

--- service/pictureUploader.php
+++ service/pictureUploader.php
@@ -8,7 +8,9 @@
         function upload($file, $filename){
             $target = $this->path.$filename;
             if(move_uploaded_file($_FILES[$file]["tmp_name"], $target)){
-                return array("result"=>0, "resultText"=>"upload success in $target");
+                return array("result"=>0, "resultText"=>$target);
+            }else{
+                return array("result"=>1, "resultText"=>"upload fail");
             }
         }
     }
