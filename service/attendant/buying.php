e599d3c7104c7f1e756c70b87d5774b410003aaf
ranion <36130413+ranion@users.noreply.github.com>
2018-03-11 15:39:48 +0700

    change ticket-list,add href in header.html

--- service/attendant/buying.php
+++ service/attendant/buying.php
@@ -1,8 +1,8 @@
 <?php
-include("loadEventID.php");
+include("loadAttendID.php");
 
 		$stmt = $connection->prepare("SELECT * FROM `event`
-			JOIN attendences on event.id=attendences.event_id WHERE `attendant_id`=$attendantID");
+			JOIN attendences on event.id=attendences.event_id WHERE `attendant_id`=$attendantID AND is_checkin = '0'");
     $stmt->execute();
     $res = $stmt->fetchAll(PDO::FETCH_OBJ);
 		 echo json_encode($res);
--- service/attendant/cancelEvent.php
+++ service/attendant/cancelEvent.php
@@ -13,6 +13,7 @@
 			$result = $statement->fetch();
 			$attendantID = $result[0];
 			echo $eventid;
+			echo $attendantID;
      $sql = "UPDATE attendences SET status_id = 'R' WHERE attendant_id = '$attendantID' AND event_id='$eventid' ";
 
      // Prepare statement
