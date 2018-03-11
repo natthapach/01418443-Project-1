1f3c8b59467e052bb4cb880e39c5dbec844df41e
natthapach <natthapach.a@ku.th>
2018-03-11 19:19:17 +0700

    complete comment
    finish send comment

--- web/attendant/js/comment.js
+++ web/attendant/js/comment.js
@@ -1,41 +1,56 @@
 function start(event_id){
     console.log(event_id);
-    
-    $(document).ready(function(e){
+
+    $("#submit-comment").click(function(e){
         $.ajax({
-            url:"../../service/attendant/getComments.php",
-            dataType:"json",
+            url:"../../service/attendant/sendComment.php",
             data:{
+                comment:$("#comment-input").val(),
                 event_id:event_id
             },
-            type:"get",
+            type:"post",
             success:function(res){
-                console.log("res", res);
-                res.forEach(comment=>{
-                    let dc = $("#comment-container").append("<div class='row comment'></div>").children().last();
-                    // let di = d.append("<div class='col-xs-4'>&nbsp;</div>").children().last();
-                    // let img = di.append('<img src="../../service/profile/'+comment.profile+'" width="100%"></img>');
-                    // let dc = d.append("<div class='col-xs-8'></div>").children().last();
-                    // dc.append("sadwsa");
-                    // d.html(comment.user_name + "<br>" + comment.content);
+                if(res!=0){
+                    // success
+                    loadComment(event_id);
+                }
 
-                    // Dream
-                    // imagePerson = '<img src="../../service/profile/'+comment.profile+'" width="200"></img>';
-                    // commentBox ='<div class="comment-box">'+imagePerson+'<h3><b>'+comment.user_name+'</b></h3><p>'+comment.content + " <br>sssssssssssssssssssssssssss<br>Sdddddddddssssssssssssssssssssssssssssssssssssssssssssssss" +'</p></div>';
-                    // d.html(commentBox);
-                    // "../../service/profile/{res.profile}"
-                    
-                    let di = dc.append("<div class='col-2 comment-image-container'></div>").children().last();
-                    let dt = dc.append("<div class='comment-box col-10 comment-content-container'></div>").children().last();
-
-                    let img = di.append("<img src='../../service/profile/" + comment.profile + "' class='comment-box-img' width='100%'>").children().last();
-                    dt.append("<h2><b>" + comment.user_name + "</b></h2>").append("<h5>" + comment.content + "</h5>")
-
-                })
-            },
-            error:function(e){
-                console.log(e);
+                console.log(res);
             }
         })
+        console.log($("#comment-input").val());
+        console.log($("#comment-input").val(""))
+    });
+    
+    $(document).ready(function(e){
+        loadComment(event_id);
+    })
+}
+
+function loadComment(event_id){
+    $.ajax({
+        url:"../../service/attendant/getComments.php",
+        dataType:"json",
+        data:{
+            event_id:event_id
+        },
+        type:"get",
+        success:function(res){
+            console.log("res", res);
+            $("#comment-container").empty();
+            res.forEach(comment=>{
+                let dc = $("#comment-container").append("<div class='row comment'></div>").children().last();
+                
+                let di = dc.append("<div class='col-xs-4 col-sm-2 comment-image-container'></div>").children().last();
+                let dt = dc.append("<div class='comment-box col-xs-8 col-sm-10 comment-content-container'></div>").children().last();
+
+                let img = di.append("<img src='../../service/profile/" + comment.profile + "' class='comment-box-img' width='100%'>").children().last();
+                dt.append("<h2><b>" + comment.user_name + "</b></h2>").append("<h5>" + comment.content.replace(/\n/g, "<br>") + "</h5>")
+
+            })
+        },
+        error:function(e){
+            console.log(e);
+        }
     })
 }
