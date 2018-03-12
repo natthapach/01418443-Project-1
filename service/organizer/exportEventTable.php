8e86d554e5f0403f36d578908626afe010da2acd
natthapach <natthapach.a@ku.th>
2018-03-12 17:47:13 +0700

    Fail!! to export event table

---
+++ service/organizer/exportEventTable.php
@@ -0,0 +1,46 @@
+<?php
+    include("../connection.php");
+    session_start();
+    // fake user name
+    $_SESSION["current_username"] = "organizer01";
+
+    $username = $_SESSION["current_username"];
+
+    $statement = $connection->prepare(
+        'select e.*, COUNT(at.event_id) as attendants
+        from event as e 
+        join organizer as o 
+        on o.id = e.organizer_id 
+        join account as a 
+        on o.user_name = a.user_name 
+        left join attendences as at
+        ON at.event_id = e.id
+        where a.user_name=:username
+        GROUP BY e.id'
+    );
+
+    $statement->execute([
+        ":username"=>$username
+    ]);
+
+    $result = $statement->fetchAll(PDO::FETCH_OBJ);
+?>
+
+<div>
+    <table id="event-table" class="table table-striped">
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>name</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php foreach($row as $result): ?>
+            <tr>
+                <td><?php echo $row->id; ?></td>
+                <td><?php echo $row->name; ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
